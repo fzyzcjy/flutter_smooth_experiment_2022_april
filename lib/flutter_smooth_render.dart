23e1e252feec42534a2d845c9216e7867b95a80a
fzyzcjy <ch271828n@outlook.com>
2022-04-03 09:56:27 +0800

    add simplest api

--- lib/flutter_smooth_render.dart
+++ lib/flutter_smooth_render.dart
@@ -1,7 +1,28 @@
-library flutter_smooth_render;
+import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class Smoother extends StatelessWidget {
+  final SmootherPlaceholder placeholder;
+  final WidgetBuilder builder;
+
+  const Smoother({
+    Key? key,
+    this.placeholder = const SmootherPlaceholder(),
+    required this.builder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TODO;
+  }
+}
+
+@immutable
+class SmootherPlaceholder {
+  final Size size;
+  final Color color;
+
+  const SmootherPlaceholder({
+    this.size = const Size(20, 20),
+    this.color = Colors.transparent,
+  });
 }
